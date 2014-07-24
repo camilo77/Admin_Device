95f80000d994a5ee233de7b3446df4268580fe70
Camilo77 <camilopineda77@hotmail.com>
2014-07-24 00:13:28 -0500

    +modelos aplicacion

--- device_app/admin.py
+++ device_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from device_app.models import Device, Application, DeviceApp
 
 # Register your models here.
+
+admin.site.register(Device)
+admin.site.register(Application)
+admin.site.register(DeviceApp)
--- device_app/models.py
+++ device_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Device(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+	ip = models.CharField(max_length=12, unique=True)
+	status = models.CharField(max_length=10)
+	tipo = models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.name
+
+
+class Application(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+
+	def __str__(self):
+		return self.name
+
+
+class DeviceApp(models.Model):
+	device = models.ForeignKey(Device)
+	aplication = models.ForeignKey(Application)
+	status = models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.device, self.aplication, self.status
